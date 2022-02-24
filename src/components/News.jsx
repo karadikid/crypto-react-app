c9f295a25cadb4009046e441b4965fa09ac59841
Gouthum Karadi <karadi@virtualnex.us>
2022-02-24 17:29:31 -0400

    Fleshed out News.jsx, also added local image for api results without images.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Select, Typography, Row, Col, Avatar, CascaderProps, Card } from "antd";
 import moment from 'moment';
-
+import demoImage from '../images/cryptonews.jpg';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
 
 const {  Text, Title } = Typography;
 const { Option } = Select;
+
+
 
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 })
@@ -22,7 +24,19 @@ const News = ({ simplified }) => {
             <a href={news.url} target="_blank" rel='noreferrer'>
               <div className='news-image-container'>
                 <Title className='news-title' level={4}>{news.name}</Title>
-
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage } alt='news'/>
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(0, 100)} ...`
+                  : news.description
+                }
+              </p>
+              <div className='provider-container'>
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt='news provider'/>
+                  <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                </div>
               </div>
             </a>
           </Card>
