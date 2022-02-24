e8363d598b75bf40d2c8d0c72a082ad6652f028a
Gouthum Karadi <karadi@virtualnex.us>
2022-02-24 16:57:29 -0400

    Completed src/News.jsx successful render.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select, Typography, Row, Col, Avatar, CascaderProps } from "antd";
+import { Select, Typography, Row, Col, Avatar, CascaderProps, Card } from "antd";
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
@@ -8,14 +8,27 @@ const {  Text, Title } = Typography;
 const { Option } = Select;
 
 const News = ({ simplified }) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 })
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 })
 
   console.log(cryptoNews);
   
+  if(!cryptoNews?.value) return 'Loading...';
+  
   return (
-    <div>
-    News
-    </div>
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className='news-card'>
+            <a href={news.url} target="_blank" rel='noreferrer'>
+              <div className='news-image-container'>
+                <Title className='news-title' level={4}>{news.name}</Title>
+
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   )
 }
 
