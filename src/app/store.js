df656e0cffced49204085edaa4209fed8989e8ed
Gouthum Karadi <karadi@virtualnex.us>
2022-02-24 11:23:21 -0400

    Successful query and data destructure with redux-toolkit, store.js cryptoApi.js, src/index.js, Homepage.js

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { classPrivateMethod } from '@babel/types';
+import { configureStore } from '@reduxjs/toolkit';
+
+import { cryptoApi } from '../services/cryptoApi'
+
+export default configureStore({
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
+})
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "aeb13f9647msh5c23adc3678b16dp1a5a00jsna1201d3d6a19",
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
