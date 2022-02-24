f393861ae72f5ee64d1166ce4ef0bbe155376b88
Gouthum Karadi <karadi@virtualnex.us>
2022-02-24 14:54:46 -0400

    Successfully created cryptoNewsApi.jsx. Next format.

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'aeb13f9647msh5c23adc3678b16dp1a5a00jsna1201d3d6a19',
+  };
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+  
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+});
+
+export const {
+    useGetCryptoNewsQuery,
+} = cryptoNewsApi;
+
