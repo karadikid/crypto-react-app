07458b5ff7dbc57a970cff3d5f78ba0149a03ac7
Gouthum Karadi <karadi@virtualnex.us>
2022-02-24 04:32:25 -0400

    Had to install react-router-dom --save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { layout, Typography, Space } from 'antd';
 
 const App = () => {
   return (
-    <div>Cryptoverse</div>
+    <div>
+        <div className="app">
+            
+        </div>
+        <div className="navbar">
+            
+        </div>
+        <div className="footer">
+            
+        </div>
+    </div>
   )
 }
 
