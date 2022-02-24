cf3e5835e08feeb3c4e8aa3a6f97792a0cae0df7
Gouthum Karadi <karadi@virtualnex.us>
2022-02-24 07:24:02 -0400

    Updated Route paths to new version ES6. Success.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import { layout, Typography, Space } from "antd";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
-import { Navbar, Layout, Footer } from "./components";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
 import "./App.css";
 
 const App = () => {
@@ -13,29 +20,19 @@ const App = () => {
           <Navbar />
         </div>
         <div className="main">
-            <Layout>
-                <div className="routes">
-                <Switch>
-                    <Route exact path="/">
-                        <Homepage />
-                    </Route>
-                    <Route exact path="/exchanges">
-                        <Exchanges />
-                    </Route>
-                    <Route exact path="/cryptocurrencies">
-                        <Cryptocurrencies />
-                    </Route>
-                    <Route exact path="/news">
-                        <News />
-                    </Route>
-                </Switch>
-
-                </div>
-            </Layout>
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/" element={<Homepage />}></Route>
+                <Route exact path="/exchanges" element={<Exchanges />}></Route>
+                <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />}></Route>
+                <Route exact path="/crypto/:coinId" element={<CryptoDetails />}></Route>
+                <Route exact path="/news" element={<News />}></Route>
+              </Routes>
+            </div>
+          </Layout>
         </div>
-        <div className="footer">
-            
-        </div>
+        <div className="footer"></div>
       </div>
     </div>
   );
