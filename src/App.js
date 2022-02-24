74510db888af6adc63431f73e370db49230bdeb2
Gouthum Karadi <karadi@virtualnex.us>
2022-02-24 06:56:15 -0400

    Added src/App.js main, Switch - path / /exchanges, /cryptocurrencies, /news etc.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
-import { layout, Typography, Space } from 'antd';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { layout, Typography, Space } from "antd";
 
-import { Navbar, Layout, Footer } from './components';
-import './App.css';
+import { Navbar, Layout, Footer } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-        <div className="app">
-
-        </div>
+      <div className="app">
         <div className="navbar">
-            <Navbar />
+          <Navbar />
+        </div>
+        <div className="main">
+            <Layout>
+                <div className="routes">
+                <Switch>
+                    <Route exact path="/">
+                        <Homepage />
+                    </Route>
+                    <Route exact path="/exchanges">
+                        <Exchanges />
+                    </Route>
+                    <Route exact path="/cryptocurrencies">
+                        <Cryptocurrencies />
+                    </Route>
+                    <Route exact path="/news">
+                        <News />
+                    </Route>
+                </Switch>
+
+                </div>
+            </Layout>
         </div>
         <div className="footer">
             
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
