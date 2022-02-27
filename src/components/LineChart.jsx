a142ff8f4d494986b369500e7bc1e796296bc495
Gouthum Karadi <karadi@virtualnex.us>
2022-02-27 07:16:28 -0400

    Sucessfully formatted and display chart. Still issues with invalid scale yAxes in DevTools Brave. Also CORS shows up after using a key for a certain amount of time. May need to change some other things such as baseURL.

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,25 +1,60 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
+import {Chart as ChartJS} from 'chart.js/auto'
+
+
 
 const { Title } = Typography;
 
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
 
-const LineChart = ({coinHistory, currentPrice, coinName}) => {
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
 
-// console.log(coinHistory?.data?.change);
-  
-return (
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+  }
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price In USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
     <>
-        <Row className='chart-header'>
-            <Title className='chart-title' level={2}>{coinName} Price Chart</Title>
-            <Col className='price-container'>
-                <Title className='price-change' level={5}>{coinHistory?.data?.change}%</Title>
-                <Title className='current-price' level={5}>Current {coinName} Price: $ {currentPrice}</Title>
-            </Col>
-        </Row>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">{coinName} Price Chart </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
+          <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
     </>
-  )
-}
+  );
+};
 
-export default LineChart
+export default LineChart;
